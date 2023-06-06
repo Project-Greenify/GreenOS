af5b7bb1bc711984c3a87b295657e90f19f11b48
HaxeFloppa <123578256+HaxeFloppa@users.noreply.github.com>
2023-06-06 07:31:54 +0100

    Update main.s

--- src/main.s
+++ src/main.s
@@ -6,12 +6,25 @@ int 0x10
 mov ah, 0Eh
 mov al, 'H'
 mov bh, 1
-mov bl, 15
+mov bl, 0
 int 0x10
 mov al, 'i'
 int 0x10
 mov al, '!'
 int 0x10
+mov ah, 0Ch
+mov cx, 0
+mov dx, 0
+.loop:
+    inc cx
+    cmp cx, 5
+    je .nl
+    int 0x10
+    jmp .loop
+.nl:
+    inc dx
+    mov cx, 0
+    jmp .loop
 jmp $
 times 510-($-$$) db 0
 dw 0AA55h
